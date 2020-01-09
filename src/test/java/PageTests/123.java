204639915cd2d3e5baeca1d890fc2ef2a8e88478
vijay <nfsvijaynfs@gmail.com>
2020-01-09 10:29:12 +0530

    123

---
+++ src/test/java/PageTests/123.java
@@ -0,0 +1,14 @@
+package PageTests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.beust.jcommander.Parameter;
+
+public class Parameters123 {
+	WebDriver driver;
+		
+}
